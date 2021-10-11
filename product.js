2703ce99a21508f964e7e9973e7497b6fe9394ce
Brett Seifried <seifriedbrett@gmail.com>
2021-10-11 13:26:07 -0700

    HTML started, lists made in JS.

---
+++ product.js
@@ -0,0 +1,30 @@
+export const shirts = [
+    {
+        id: '1',
+        name: 'Gold Dragon on Black', 
+        img: './assets/shirt.webp', 
+        Drip: 60,
+        price: 100
+    },
+    {
+        id: '2',
+        name: 'Gold flowers on Red', 
+        img: './assets/shirt1.jpeg', 
+        Drip: 40,
+        price: 125
+    },
+    {
+        id: '3',
+        name: 'Gold', 
+        img: './assets/shirt2.webp', 
+        Drip: 100,
+        price: 175
+    },
+    {
+        id: '4',
+        name: 'Red flowers on Black', 
+        img: './assets/shirt4.jpeg', 
+        Drip: 15,
+        price: 80
+    },
+];
