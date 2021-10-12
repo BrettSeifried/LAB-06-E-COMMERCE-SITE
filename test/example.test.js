1ffd62debb21328493cc2b4ef1801a7a9114e812
Brett Seifried <seifriedbrett@gmail.com>
2021-10-12 16:11:04 -0700

    styled cart more, fixed TDD test, added progress on calOrderTotal TDD

--- test/example.test.js
+++ test/example.test.js
@@ -9,7 +9,7 @@ const test = QUnit.test;
 test('renderShirt should return an HTML snippet', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="shirt-card"><h2>Gold Arm</h2><img src="./assets/shirt.webp"><h3>Gold sleeve on black shirt.</h3><h4>UFC</h4><p>$100</p><button>Add</button></div>`;
+    const expected = `<div class="shirt-card"><h2>Gold Arm</h2><img src="./assets/shirt.webp"><h3>Gold sleeve on black shirt.</h3><h4>UFC</h4><p>100</p><button>Add</button></div>`;
     const shirt1 = shirts [0];
     //Act 
     // Call the function you're testing and set the result to a const
@@ -20,17 +20,21 @@ test('renderShirt should return an HTML snippet', (expect) => {
     expect.equal(actual, expected);
 });
 
-test('findById shoudl return the item matching the ID', (expect) => {
+test('findById should return the item matching the ID', (expect) => {
     const expected = {
         id: '4',
         name: 'Red Flowers', 
         img: './assets/shirt4.jpg', 
         description: 'Red flowers on see through black shirt',
         category: 'Party',
-        price: '$80',
+        price: 80,
         button: 'Add'
     };
 
     const actual = findById('4', shirts);
     expect.deepEqual(actual, expected);
 });
+
+// test('test calOrderTotal should come back as a total', (expect) => {
+//     const expected = 
+// });
